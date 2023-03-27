d1a3a71d489ecace2f86f040093978caa815ede5
andreaferritti <andrea.ferritti@stud.unifi.it>
2023-03-27 17:29:02 +0200

    Added version name

---
+++ Version.h
@@ -0,0 +1,14 @@
+//
+// Created by Andrea Ferritti on 27/03/23.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+#include <string>
+
+static const std::string version = "v.1.0";
+
+
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
